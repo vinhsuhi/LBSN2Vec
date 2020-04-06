bca17917ef5d316ad0837086054d03b675790598
Thanh Dat <dd.tt97@gmail.com>
2020-04-07 00:34:16 +0700

    Add several functions

--- experiment.py
+++ experiment.py
@@ -2,7 +2,21 @@ from scipy.io import loadmat
 from utils import random_walk, create_social_graph, get_neg_sample, load_data
 from tqdm import tqdm
 import numpy as np
+import math
 
+RAND_MULTIPLIER = 25214903917
+RAND_INCREMENT = 11
+MAX_EXP = 6
+EXP_TABLE_SIZE = 1000
+ULONG_MAX = 18446744073709551615
+
+# need initializing
+mobility_ratio = 0.
+dim_emb = 0
+emb_n = []
+neg_sam_table_social = None
+table_size_social = None
+num_neg = 0
 
 def fixed_unigram_candidate_sampler(num_sampled, unique, range_max, distortion, unigrams):
     weights = unigrams**distortion
@@ -23,35 +37,219 @@ def getNextRand():
     """
     TODO: Define global next_random
     """
-    pass
+    return RAND_MULTIPLIER + RAND_INCREMENT
 
-
-def get_a_social_decision():
-    rand = np.random.rand()
-    return rand > MOBILITYRATIO
-
-
-def get_a_neg_sample():
+def get_a_neg_sample(next_random, neg_sam_table, table_size):
     """
     TODO: IMPLEMENT THIS
     """
-    rand = np.random.rand()
-    # if 
-    pass
+    ind = (next_random >> 16) % table_size
+    target_n = neg_sam_table[ind]
+    return target_n
 
+def get_a_checkin_sample(next_random, table_size):
+    return (next_random >> 16) % table_size
 
-def learn_an_edge_with_BFT(word, target_e, best_fit):
+def sigmoid(f):
+    if f >= MAX_EXP:
+        return 1
+    elif f <= -MAX_EXP: return 0
+    else:
+        return expTable[int((f + MAX_EXP) * (EXP_TABLE_SIZE / MAX_EXP / 2 ))]
+
+def get_a_neg_sample_Kless1(next_random):
+    v_rand_uniform = next_random / ULONG_MAX
+    if v_rand_uniform <= num_neg:
+        return 1
+    else:
+        return 0
+
+def get_a_social_decision(next_random):
+    v_rand_uniform = next_random / ULONG_MAX
+    if v_rand_uniform <= mobility_ratio:
+        return 0
+    else:
+        return 1
+
+def get_a_mobility_decision(next_random):
+    v_rand_uniform = next_random / ULONG_MAX
+    if v_rand_uniform <= mobility_ratio:
+        return 1
+    else:
+        return 0
+
+def get_norm_l2_loc(loc_node):
+    norm = 0
+    for d in range(dim_emb):
+        norm = norm + emb_n[loc_node+d] * emb_n[loc_node+d]
+    return math.sqrt(norm)
+
+def get_norm_l2_pr(vec):
+    norm = 0
+    for d in range(dim_emb):
+        norm = norm + vec[d]**2
+    return math.sqrt(norm)
+        
+def learn_a_pair_loc_loc_cosine(flag, loc1, loc2, loss):
+    f = 0
+    tmp1, tmp2, c1, c2, c3 = 0, 0, 0, 0, 0
+    norm1 = get_norm_l2_loc(loc1)
+    norm2 = get_norm_l2_loc(loc2)
+
+    for d in range(dim_emb):
+        f += emb_n[loc1+d] * emb_n[loc2+d]
+    c1 = 1/(norm1*norm2)*alpha
+    c2 = f/(norm1**3 * norm2) * alpha
+    c3 = f/(norm1*norm2**3) * alpha
+
+    if flag == 1:
+        for d in range(dim_emb):
+            tmp1 = emb_n[loc1+d]
+            tmp2 = emb_n[loc2+d]
+            emb_n[loc2+d] += c1*tmp1 - c3*tmp2
+            emb_n[loc1+d] += c1*tmp2 - c2*tmp1
+    else:
+        for d in range(dim_emb):
+            tmp1 = emb_n[loc1+d]
+            tmp2 = emb_n[loc2+d]
+            emb_n[loc2+d] -= c1*tmp1 - c3*tmp2
+            emb_n[loc1+d] -= c1*tmp2 - c2*tmp1
+
+def learn_a_pair_loc_pr_cosine(flag, loc1, best_fit, loss):
+    f, g, a, c1, c2 = 0, 0, 0, 0, 0
+    norm1 = get_norm_l2_loc(loc1)
+    for d in range(dim_emb):
+        f += emb_n[loc1+d]*best_fit[d]
+
+    g = f/norm1
+    a = alpha
+    c1 = 1/(norm1)*a
+    c2 = f/(norm1*norm1*norm1)*a
+    if flag == 1:
+        for d in range(dim_emb):
+            emb_n[loc1+d] += c1*best_fit[d] - c2*emb_n[loc1+d]
+    else:
+        for d in range(dim_emb):
+            emb_n[loc1+d] -= c1*best_fit[d] - c2*emb_n[loc1+d]
+
+def learn_an_edge(word, target_e, next_random, counter):
+    target_n, loc_neg = 0,0 
+    loc_w = (word-1)*dim_emb
+    loc_e = (target_e-1)*dim_emb
+    learn_a_pair_loc_loc_cosine(1, loc_w, loc_e, counter)
+
+    if num_neg < 1:
+        next_random = getNextRand(next_random)
+        if get_a_neg_sample_Kless1(next_random) == 1:
+            next_random = getNextRand(next_random)
+            target_n = get_a_neg_sample(next_random, neg_sam_table_social, table_size_social)
+            if target_n != target_e and target_n != word:
+                loc_neg = (target_n-1) * dim_emb
+                learn_a_pair_loc_loc_cosine(0, loc_w, loc_neg, counter)
+    else:
+        for i in range(num_neg):
+            next_random = getNextRand(next_random)
+            target_n = get_a_neg_sample(next_random, neg_sam_table_social, table_size_social)
+            if target_n != target_e and target_n != word:
+                loc_neg = (target_n-1)*dim_emb
+                learn_a_pair_loc_loc_cosine(0, loc_w, loc_neg, counter)
+
+# def get_a_social_decision():
+#     rand = np.random.rand()
+#     return rand > MOBILITYRATIO
+
+def learn_an_edge_with_BFT(word, target_e, best_fit, counter):
     """
     TODO: IMPLEMENT THIS
     WHAT DOES THIS FUNCTION DO?
     """
-    negs = fixed_unigram_candidate_sampler(KNEG, False, range_max, 0.75, unigrams):
+    # negs = fixed_unigram_candidate_sampler(KNEG, False, range_max, 0.75, unigrams):
 
-    for n in range(KNEG):
-        # TODO: implement this function
-        target_n = get_a_neg_sample()
+    # for n in range(KNEG):
+    #     # TODO: implement this function
+    #     target_n = get_a_neg_sample()
+    target_n, loc_neg = 0,0
+    norm = 0
+    loc_w = (word-1) * dim_emb
+    loc_e = (target_e-1)*dim_emb
+    for d in range(dim_emb):
+        best_fit[d] = emb_n[loc_w+d] + emb_n[loc_e+d]
+    norm = get_norm_l2_pr(best_fit)
+    for d in range(dim_emb):
+        best_fit[d] = best_fit[d]/norm
+    
+    learn_a_pair_loc_pr_cosine(1, loc_w, best_fit, counter)
+    learn_a_pair_loc_pr_cosine(1, loc_e, best_fit, counter)
 
+    if num_neg < 1:
+        next_random = getNextRand(next_random)
+        if get_a_neg_sample_Kless1(next_random) == 1:
+            next_random = getNextRand(next_random)
+            target_n = get_a_neg_sample(next_random, neg_sam_table_social, table_size_social)
+            if target_n != target_e and target_n != word:
+                loc_neg = (target_n-1)*dim_emb
+                learn_a_pair_loc_pr_cosine(0, loc_neg, best_fit, counter)
+    else:
+        for n in range(num_neg):
+            next_random = getNextRand(next_random)
+            target_n = get_a_neg_sample(next_random, neg_sam_table_social, table_size_social)
+            if target_n != target_e and target_n != word:
+                loc_neg = (target_n-1)*dim_emb
+                learn_a_pair_loc_pr_cosine(0, loc_neg, best_fit, counter)
 
+def learn_a_hyperedge(edge, edge_len, best_fit, counter):
+    node, target_neg = 0,0
+    loc_n, loc_neg = 0,0 
+    norm = 0
+    for d in range(dim_emb):
+        best_fit[d] = 0
+    for i in range(edge_len):
+        loc_n = (edge[i]-1)*dim_emb
+        norm = get_norm_l2_pr(emb_n[loc_n])
+        for d in range(dim_emb):
+            best_fit[d] += emb_n[loc_n+d] / norm
+    norm = get_norm_l2_pr(best_fit)
+    for d in range(dim_emb):
+        best_fit[d] = best_fit[d]/norm
+    
+    for i in range(edge_len):
+        node = edge[i]
+        loc_n = (node-1)*dim_emb
+        learn_a_pair_loc_pr_cosine(1, loc_n, best_fit, counter)
+
+        if num_neg < 1:
+            next_random = getNextRand(next_random)
+            if get_a_neg_sample_Kless1(next_random) == 1:
+                next_random = getNextRand(next_random)
+                if i == 0:
+                    target_neg = get_a_neg_sample(next_random, neg_sam_table_mobility1, table_size_mobility1)
+                elif i == 1:
+                    target_neg = get_a_neg_sample(next_random, neg_sam_table_mobility2, table_size_mobility2)
+                elif i == 2:
+                    target_neg = get_a_neg_sample(next_random, neg_sam_table_mobility3, table_size_mobility3)
+                elif i == 3:
+                    target_neg = get_a_neg_sample(next_random, neg_sam_table_mobility4, table_size_mobility4)
+                
+                if target_neg != node:
+                    loc_neg = (target_neg-1)*dim_emb
+                    learn_a_pair_loc_pr_cosine(0, loc_neg, best_fit, counter)
+        else:
+            for n in range(num_neg):
+                next_random = getNextRand(next_random)
+                if i == 0:
+                    target_neg = get_a_neg_sample(next_random, neg_sam_table_mobility1, table_size_mobility1)
+                elif i == 1:
+                    target_neg = get_a_neg_sample(next_random, neg_sam_table_mobility2, table_size_mobility2)
+                elif i == 2:
+                    target_neg = get_a_neg_sample(next_random, neg_sam_table_mobility3, table_size_mobility3)
+                elif i == 3:
+                    target_neg = get_a_neg_sample(next_random, neg_sam_table_mobility4, table_size_mobility4)
+                if target_neg != node:
+                    loc_neg = (target_neg-1)*dim_emb
+                    learn_a_pair_loc_pr_cosine(0, loc_neg, best_fit, counter)
+
+def merge_hyperedges(edge_merged, edge_merged_len, a_edge, a_edge_len):
+    
 
 if __name__ == "__main__":
     NUMWALK = 1
