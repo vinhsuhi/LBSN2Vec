75fe3ccddfeb2a0cb61f7a2ac54427ff5e4c3f39
vinhsuhi <vinhbachkhoait@gmail.com>
2020-11-29 19:30:22 +0700

    update script

--- script_location.sh
+++ script_location.sh
@@ -104,7 +104,7 @@ do
     do
         for pp in 0.6 0.8 1
         do 
-            python -u CMan_POI.py --input_type ${mode} --dataset_name ${data} --bias_randomwalk --p_n2v ${pp} --q_n2v ${qq} --workers 32 > output1/${data}_p${pp}_q${qq}_POI
+            python -u CMan_POI.py --input_type ${mode} --dataset_name ${data} --bias_randomwalk --p_n2v ${pp} --q_n2v ${qq} --workers 32 --mobility_ratio 0.7 > output1/${data}_p${pp}_q${qq}_POI
         done 
     done
 done
@@ -112,22 +112,27 @@ done
 
 mode="persona_POI"
 
-for data in SaoPaulo 
+for p in 0.8
 do
-    for lr in 0.0005 
-    do 
-        for dim in 300
-        do 
-            for Kneg in 15 20
-            do 
-                python -u CMan_POI.py --input_type ${mode} --dim_emb ${dim} \
-                --dataset_name ${data} --bias_randomwalk --p_n2v 0.8 --q_n2v 0.8 \
-                --workers 32 --learning_rate ${lr} --K_neg ${Kneg} --workers 12 > sao_output/${dim}_${lr}_${Kneg}
-            done
-        done 
-    done
+for q in 0.6 0.8 1
+do
+for lr in 0.0005 0.001
+do
+for dim in 256 300
+do
+for Kneg in 5 10
+do
+for data in Jakarta KualaLampur SaoPaulo
+do
+python -u CMan_POI.py --input_type ${mode} --dim_emb ${dim} \
+--dataset_name ${data} --bias_randomwalk --p_n2v ${p} --q_n2v ${q} \
+--workers 12 --learning_rate ${lr} --K_neg ${Kneg} --mobility_ratio 0.7 --add_flag 0.9 > location_pred2/${data}_${dim}_${lr}_${Kneg}_p${p}_q${q}m0.7
 done
-
+done 
+done
+done
+done
+done
 
 
 mode="persona_POI"
